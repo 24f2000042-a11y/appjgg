c0495b3bd029203bcc4bba53e6c1d19b2c40f8b0
24f2000042-a11y <24f2000042@ds.study.iitm.ac.in>
2026-05-21 00:53:39 +0530

    update backend/controllers/authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,21 +1,17 @@
 const User = require('../models/User');
-const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
-  const { email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = new User({ email, password: hashedPassword });
-  await user.save();
-  res.status(201).send('User registered.');
+  const { username, password } = req.body;
+  const newUser = new User({ username, password });
+  await newUser.save();
+  res.status(201).send('User created.');
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user) return res.status(400).send('Invalid email or password.');
-  const validPassword = await bcrypt.compare(password, user.password);
-  if (!validPassword) return res.status(400).send('Invalid email or password.');
+  const { username, password } = req.body;
+  const user = await User.findOne({ username, password });
+  if (!user) return res.status(400).send('Invalid credentials.');
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
   res.json({ token });
 };
