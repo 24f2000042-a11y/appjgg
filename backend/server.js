5ff8cd7eab789b54bba89a1b280bb60bec6c0313
24f2000042-a11y <24f2000042@ds.study.iitm.ac.in>
2026-05-21 00:53:31 +0530

    update backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/itemRoutes');
 
 dotenv.config();
 
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.send('API is running...'));
 app.get('/api/health', (req, res) => res.send('Healthy'));
-app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
