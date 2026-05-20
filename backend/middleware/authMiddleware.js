4213a7d2f647a64cf17777a0a69027d001cc33b2
24f2000042-a11y <24f2000042@ds.study.iitm.ac.in>
2026-05-21 00:53:37 +0530

    update backend/middleware/authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers['authorization']?.split(' ')[1];
-  if (!token) return res.status(401).send('Access denied. No token provided.');
+  const token = req.headers['authorization'];
+  if (!token) return res.status(401).send('Access denied.');
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.status(403).send('Invalid token.');
     req.user = user;
